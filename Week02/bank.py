22aa0ba65cbfb0f3331d791e738977f5508a426f
Neil Fitzgerald <neil.fitzgerald@outlook.com>
2023-02-01 23:58:49 +0000

    week 02

--- Week02/bank.py
+++ Week02/bank.py
@@ -2,8 +2,6 @@
 #Week 2: This program prompts the user to input two amounts in cents and outputs the answer in euro.cent format. 
 #author: Neil Fitzgerald 
 
-
-
 # The user is prompted to enter in the first amount (cents)
 first_amount = int(input("Enter the first amount in cents: "))
 
@@ -13,7 +11,7 @@ second_amount = int(input("Enter the second amount in cents: "))
 # Both user input amounts are added together
 total_amount = first_amount + second_amount
 
-# The answer is formatted into euro.cent by diving the total amount as an integer '//' (euro) and remainder '%' (cents)
+# The answer is formatted into euro.cent by diving the total amount as an integer '//' (euro) and remainder '%' (cent)
 euro = total_amount // 100
 cent = total_amount % 100
 
